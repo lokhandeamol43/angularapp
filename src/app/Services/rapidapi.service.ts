a5c0b003a9019dc22f0466a4ae0fe60a0468c84a
lokhandeamol43 <lokhandeamol43@gmail.com>
2023-09-05 22:56:49 +0530

    remove api

--- src/app/Services/rapidapi.service.ts
+++ src/app/Services/rapidapi.service.ts
@@ -6,25 +6,6 @@ import { Injectable } from '@angular/core';
 })
 export class RapidapiService {
 
-  url = 'https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=tesla&region=US';
 
-  url2 = 'https://country-state-city-search-rest-api.p.rapidapi.com/allcountries';
-  constructor(private _httpClient: HttpClient) { }
-
-  GetYahooFinance() {
-    let headers = new HttpHeaders({
-      'X-RapidAPI-Key': '9b806c102emshddc858756e4325bp182ac5jsn590f1e4b256a',
-       'X-RapidAPI-Host': 'apidojo-yahoo-finance-v1.p.rapidapi.com'
-    })
-   return this._httpClient.get(this.url, {headers: headers});
-  }
-
-  GetCountryData(){
-    let headers1 = new HttpHeaders({
-      'X-RapidAPI-Key': '7f4013b5c5mshca386355f315c38p1b1333jsnb074d3ba0829',
-       'X-RapidAPI-Host': 'country-state-city-search-rest-api.p.rapidapi.com'
-    })
-    return this._httpClient.get(this.url2, {headers:headers1})
-  }
 
 }
